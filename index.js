f5b7fa9f1038073308318917275c47cbb0690d40
Leandro-Pinho <leandroessencia@gmail.com>
2023-05-19 22:23:39 -0300

    adicionando responsividade

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ----------- função para aparece o menuSuspenso -------
 var menuSuspenso = document.getElementById("menuProduto");
 
 
@@ -8,3 +9,17 @@ function pEnter() {
 function pClose() {
     menuSuspenso.style.display = 'none'
 }
+
+// -------------- function para o menu na vertical quando a tela esta pequena--------------
+var MenuItems = document.getElementById('menuItems');
+
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle() {
+    if (MenuItems.style.maxHeight == "0px") {
+        MenuItems.style.maxHeight = "200px"
+    } else {
+        MenuItems.style.maxHeight = "0px"
+    }
+}
+
